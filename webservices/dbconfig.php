6cfaace00313d4c096c371187f0d6237e4612639
kvesskrishna <krishna.kkarthi@gmail.com>
2018-03-21 08:31:10 +0530

    test
    tse

--- webservices/dbconfig.php
+++ webservices/dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-//Open a new connection to the MySQL server
+//Open a new connection to the MySQL servers
 $mysqli = new mysqli('localhost','kpiasdot_dev17','root@KPIAS123','kpiasdot_dev17');
 //Output any connection error
 if ($mysqli->connect_error) {
